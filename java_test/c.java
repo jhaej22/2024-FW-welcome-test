d9bc42e67ba772fb8bfd07e622e22962c9c10a79
haejoon <jhaej0722@gmail.com>
2024-08-19 08:23:34 +0900

    c commit

--- java_test/c.java
+++ java_test/c.java
@@ -1,10 +1,23 @@
 package java_test;
 
+import java.io.*;
+
 public class c {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int age = Integer.parseInt(br.readLine());
+        int height = Integer.parseInt(br.readLine());
 
-        // 이 부분에 code를 작성해주세요!
+        if (age >= 14 || height >= 160) {
+            bw.write("X");
+        } else {
+            bw.write("O");
+        }
 
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
 }
