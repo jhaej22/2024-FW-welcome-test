575e11ea3bc0e13fbd28c8e27683319adc33f552
haejoon <jhaej0722@gmail.com>
2024-08-19 09:07:15 +0900

    e commit

--- java_test/e.java
+++ java_test/e.java
@@ -1,10 +1,26 @@
 package java_test;
 
+import java.io.*;
+
 public class e {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        int month = Integer.parseInt(br.readLine());
+        int day = switch (month) {
+            case 4, 6, 9, 11 -> 30;
+            case 2 -> checkLeapYear(year);
+            default -> 31;
+        };
 
-        // 이 부분에 code를 작성해주세요!
+        System.out.println(day);
+    }
 
+    private static int checkLeapYear(int y) {
+        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+            return 29;
+        }
+        return 28;
     }
 
 }
