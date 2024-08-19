d492e9a0d7bd31131e3747ec23a59e2c732cf9db
haejoon <jhaej0722@gmail.com>
2024-08-19 09:33:41 +0900

    f1 commit

--- java_test/f1.java
+++ java_test/f1.java
@@ -1,10 +1,56 @@
 package java_test;
 
+import java.io.*;
+
 public class f1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Fan[] fans = new Fan[3];
 
-        // 이 부분에 code를 작성해주세요!
+        for (int i = 0; i < 3; i++) {
+            String model = br.readLine();
+            int price = Integer.parseInt(br.readLine());
+            fans[i] = new Fan(model, price);
+        }
 
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (fans[i].getPrice() < min) {
+                min = fans[i].getPrice();
+                index = i;
+            }
+        }
+
+        System.out.println(fans[index].getModel());
+    }
+
+    private static class Fan {
+        private  String model;
+        private  int price;
+
+        public Fan(String model, int price) {
+            this.model = model;
+            this.price = price;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+
+        public int getPrice() {
+            return price;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public void setPrice(int price) {
+            this.price = price;
+        }
     }
 
 }
